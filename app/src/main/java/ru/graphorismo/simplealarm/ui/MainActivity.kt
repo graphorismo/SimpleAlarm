963b571fd961c474fcc88fa01a87cec5ad5b4bbe
graphorismo <graphorismo@gmail.com>
2022-12-06 08:54:41 +0300

    Move MainActivity.kt into ui package. Lock screen orientation. Set up basic layout.

--- app/src/main/java/ru/graphorismo/simplealarm/ui/MainActivity.kt
+++ app/src/main/java/ru/graphorismo/simplealarm/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package ru.graphorismo.simplealarm
+package ru.graphorismo.simplealarm.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.graphorismo.simplealarm.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
